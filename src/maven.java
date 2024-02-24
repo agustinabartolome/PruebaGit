46ffe8e78e132ea5566d4c3c79420d995e56306e
Agustina Bartolomé <agustina.bartolome11@gmail.com>
2024-02-24 17:27:15 -0300

    Se suma un mensaje

--- src/maven.java
+++ src/maven.java
@@ -4,6 +4,8 @@ public class maven {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo!");
+		
+		System.out.println("Este mensaje es nuevo");
 	}
 
 }
